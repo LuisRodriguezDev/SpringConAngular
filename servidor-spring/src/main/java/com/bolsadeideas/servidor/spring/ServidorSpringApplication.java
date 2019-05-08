1e46c78f767e7c039a85a594a6496f9219c5ae8d
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-08 19:35:35 -0300

    Crear servidor con spring

---
+++ servidor-spring/src/main/java/com/bolsadeideas/servidor/spring/ServidorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.servidor.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorSpringApplication.class, args);
+	}
+
+}
