239cfa4c29304fafa5b61a3853278f035f1bf8cf
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-08 13:04:33 -0300

    Crear archivo JSON con datos de clientes de prueba

---
+++ cliente-angular/src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 2, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 3, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 4, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 5, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 6, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 7, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 8, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+  { id: 9, nombre: "Luis", apellido: "Rodriguez", email: "lrod13@hotmail.es", createdAt: "2018-05-07" },
+];
