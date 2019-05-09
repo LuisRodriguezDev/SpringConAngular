6983769f493ca575599d9c4eb608b3d08f046dac
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-09 12:07:42 -0300

    Añadir metodo findAll()

--- servidor-spring/src/main/java/com/bolsadeideas/servidor/spring/models/services/ClienteServiceImpl.java
+++ servidor-spring/src/main/java/com/bolsadeideas/servidor/spring/models/services/ClienteServiceImpl.java
@@ -3,11 +3,13 @@ package com.bolsadeideas.servidor.spring.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bolsadeideas.servidor.spring.models.dao.IClienteDAO;
 import com.bolsadeideas.servidor.spring.models.entity.Cliente;
 
+@Service
 public class ClienteServiceImpl implements IClienteService {
 
 	@Autowired
